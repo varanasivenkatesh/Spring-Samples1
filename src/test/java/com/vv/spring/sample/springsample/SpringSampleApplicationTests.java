c18e397f1e6456b22dda1d6eca0c17389e43b37c
venkateshvaranasi <varanasivenkatesh@gmail.com>
2019-12-09 19:05:36 +0530

    Initial commit

---
+++ src/test/java/com/vv/spring/sample/springsample/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vv.spring.sample.springsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
